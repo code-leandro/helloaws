e80f476f506cd514b5dc8b1d9c44b622079bd72b
Leandro Souza <leandrosouza@MacBook-Air-de-Leandro.local>
2024-09-22 00:25:32 -0300

    feat: controller developed / endpoint hello

---
+++ src/test/java/com/leandrosouza/helloaws/HelloawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandrosouza.helloaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
